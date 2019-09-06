fdbc80969147a47c804ca8f358d08f883924ceb8
araki-takahiro <sanju13gh60ki@gmail.com>
2019-09-06 10:20:31 +0000

    currentlocation and datalocation display

--- app/controllers/public/locations_controller.rb
+++ app/controllers/public/locations_controller.rb
@@ -1,11 +1,14 @@
 class Public::LocationsController < Public::ApplicationController
     def show
-        @locations = current_user.locations.all
-        @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
-            marker.lat location.latitude
-            marker.lng location.longitude
-            marker.infowindow location.location_name
-        end
+        @locations = current_user.locations.all.to_json
+        @location = current_user.locations.first.to_json.html_safe
+        # @latitudes = @locations.map{|location| location.latitude}
+        # @longitudes = @locations.map{|location| location.longitude}
+        # @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
+        #     marker.lat location.latitude
+        #     marker.lng location.longitude
+        #     marker.infowindow location.location_name
+        # end
     end
 
     def create
