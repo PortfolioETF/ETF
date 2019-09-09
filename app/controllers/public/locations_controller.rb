95de6d39f7661e8d42587d7c02a0ea9ec6f1c936
araki-takahiro <sanju13gh60ki@gmail.com>
2019-09-09 15:28:04 +0000

    tweak

--- app/controllers/public/locations_controller.rb
+++ app/controllers/public/locations_controller.rb
@@ -3,6 +3,11 @@ class Public::LocationsController < Public::ApplicationController
         render json: params[:latlong]
     end
 
+    def show
+        @smoking_posts = Location.find(params[:id]).smoking_posts
+        render json: @smoking_posts
+    end
+
     def smoking_search
         @q  = Location.ransack(params[:q])
         @result = @q.result
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
       resources :opinions, only: %i(new create)
       resource :withdraws, only: %i(show create)
     end
-    resources :locations, only: %i(new)
+    resources :locations, only: %i(new show)
     get '/smoking_search' => 'locations#smoking_search',as: 'smoking_search'
     resources :smoking_posts, except: %i(new)
   end
