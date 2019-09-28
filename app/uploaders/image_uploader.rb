a1e254678a37780c4528b811df72b46a3f5f05b1
araki-takahiro <sanju13gh60ki@gmail.com>
2019-09-28 16:02:07 +0000

    carrierwave.rb change

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,9 +8,6 @@ class ImageUploader < ApplicationUploader
         storage :fog
     end
 
-    def store_dir
-        "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    end
 
     def extension_whitelist
         %w(jpg jpeg gif png)
