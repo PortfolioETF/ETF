e57896d776cc3427455c99a4325f7b59af8d918c
araki-takahiro <sanju13gh60ki@gmail.com>
2019-09-28 15:06:24 +0000

    image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,23 @@
 class ImageUploader < ApplicationUploader
+
+    if Rails.env.development?
+        storage :file
+    elsif Rails.env.test?
+        storage :file
+    else
+        storage :fog
+    end
+
+    def store_dir
+        "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
+
     def extension_whitelist
         %w(jpg jpeg gif png)
+    end
+
+    def filename
+        original_filename if original_filename
     end
 
     def accept_extensions
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: 'AKIAW4NISNVNDOPRDEXD',
+      aws_secret_access_key: '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',
+      region: 'ap-northeast-1a'
+    }
+
+    config.fog_directory  = 'etf-image'
+    config.cache_storage = :fog
+  end
